10a451489772e9034f0ad57ce74cc83d7dbc29a8
arduino731 <arduino731@gmail.com>
2018-08-11 19:13:38 -0500

    config

---
+++ config/database.js
@@ -0,0 +1,7 @@
+// config/database.js
+module.exports = {
+
+	'url' : 'mongodb://vanvlymenlist:vanvlymenlist@ds041556.mlab.com:41556/vanvlymenlist',
+    'localhost' : 'mongodb://localhost/vanvlymenlist',
+    'port': 8000
+};
